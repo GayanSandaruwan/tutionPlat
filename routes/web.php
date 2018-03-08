613088a798a666e5898732a131e285ff75d3bda4
gayansandaruwan <gayankavirathne.14@cse.mrt.ac.lk>
2018-03-08 23:47:10 +0530

    Registration only allowed for the admins only

--- routes/web.php
+++ routes/web.php
@@ -96,8 +96,6 @@ Route::group(['prefix' => 'admin'], function () {
   Route::post('/login', 'AdminAuth\LoginController@login');
   Route::post('/logout', 'AdminAuth\LoginController@logout')->name('logout');
 
-  Route::get('/register', 'AdminAuth\RegisterController@showRegistrationForm')->name('register');
-  Route::post('/register', 'AdminAuth\RegisterController@register');
 
   Route::post('/password/email', 'AdminAuth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
   Route::post('/password/reset', 'AdminAuth\ResetPasswordController@reset')->name('password.email');
@@ -105,6 +103,8 @@ Route::group(['prefix' => 'admin'], function () {
   Route::get('/password/reset/{token}', 'AdminAuth\ResetPasswordController@showResetForm');
 
     Route::group(['middleware'=>['web','admin',]],function() {
+        Route::get('/register', 'AdminAuth\RegisterController@showRegistrationForm')->name('register');
+        Route::post('/register', 'AdminAuth\RegisterController@register');
 
         Route::get('/students', 'AdminFunctions@students');
         Route::post('/students{student_id}', 'AdminFunctions@toggleStateStudents');
